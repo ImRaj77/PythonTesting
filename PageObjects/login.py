bb8779cc25d18211d0759d122594b7178a614aaf
ImRaj77 <rajtidke77@gmail.com>
2026-01-21 12:14:24 +0530

    Updated code with Page Object Model and added .gitignore file

---
+++ PageObjects/login.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+from PageObjects.shop import ShopPage
+
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.username_input = (By.ID, "username")
+        self.password = (By.NAME, "password")
+        self.sign_button = (By.ID, "signInBtn")
+
+    def login(self, username, password):
+        self.driver.find_element(*self.username_input).send_keys(username)
+        self.driver.find_element(*self.password).send_keys(password)              # Learning@830$3mK2
+        self.driver.find_element(*self.sign_button).click()
+        shopPage = ShopPage(self.driver)
+        return shopPage
--- PythonSelenium/test_e2eTestFramework.py
+++ PythonSelenium/test_e2eTestFramework.py
@@ -1,33 +1,31 @@
-from selenium import webdriver
+import json
+import os.path
+import sys
+
+import pytest
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # Chrome driver
-from selenium.webdriver.chrome.service import Service
 # -- Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium .webdriver.support.wait import WebDriverWait
 
+from PageObjects.login import LoginPage
+test_data_path = '../data/test_e2eTestFramework.json'
+with open(test_data_path) as f:
+    test_data = json.load(f)
+    test_list = test_data["data"]
 
-def test_e2e(browserInstance):
+@pytest.mark.parametrize("test_list_item", test_list)
+def test_e2e(browserInstance, test_list_item):
     driver = browserInstance
     driver.get("https://rahulshettyacademy.com/loginpagePractise/")
-    driver.find_element(By.ID, "username").send_keys("rahulshettyacademy")
-    driver.find_element(By.NAME, "password").send_keys("Learning@830$3mK2")
-    driver.find_element(By.ID, "signInBtn").click()
-    driver.find_element(By.CSS_SELECTOR, "a[href*='shop']").click()
-    products = driver.find_elements(By.XPATH, "//div[@class='card h-100']")
+    login_page = LoginPage(driver)
+    shop_page = login_page.login(test_list_item["userEmail"], test_list_item["userPassword"])
+    shop_page.add_product_to_cart(test_list_item["productName"])
 
-    for product in products:
-        productName = product.find_element(By.XPATH, "//div/h4/a").text
-        if productName == "Blackberry":
-            product.find_element(By.XPATH, "div/button").click()
-
-    driver.find_element(By.CSS_SELECTOR, "a[class*='btn-primary']").click()
-    driver.find_element(By.XPATH, "//button[@class='btn btn-success']").click()
-    driver.find_element(By.ID, "country").send_keys("ind")
-    wait = WebDriverWait(driver, 10)
-    wait.until(expected_conditions.presence_of_element_located((By.LINK_TEXT, "India")))
-    driver.find_element(By.LINK_TEXT, "India").click()
-    driver.find_element(By.XPATH, "//div[@class='checkbox checkbox-primary']").click()
-    driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
-    successText = driver.find_element(By.CLASS_NAME, "alert-success").text
-    assert "Success! Thank you!" in successText
+    checkout_confirmation = shop_page.go_to_cart()
+    checkout_confirmation.checkout()
+    checkout_confirmation.enter_delivery_address("India")
+    checkout_confirmation.validate_order()
